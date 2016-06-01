61bc491389dc13aa87e5d997ea95299d91b5ed69
Marek <marecl@users.noreply.github.com>
2016-06-01 18:12:23 +0200

    Added scheme
    Removed almost everything about old display.
    Preparing t use PCD8544
    Added ohmmeter

--- BOXduino.ino
+++ BOXduino.ino
@@ -12,6 +12,7 @@ const int fire = 2; //Przycisk
 const int mosfet = 6; //Wyjscie (PWM)
 const int pot = A0; //Potencjometr
 const int volt = A1; //Woltomierz Vin
+const int ohmmet = A2; //Omomierz
 
 int mode = 0; //0 - VV/VW, 1 - BYPASS
 int bouncedelay = 100;
@@ -97,6 +98,8 @@ void setup() {
   pinMode(volt, INPUT);
   pinMode(fire, INPUT);
   pinMode(mosfet, OUTPUT);
+  pinMode(ohmmet, INPUT);
+  Serial.begin(19200);
   //lcd.createChar(0, full);
   //lcd.createChar(1, hi);
   //lcd.createChar(2, med);
@@ -111,8 +114,12 @@ void loop() {
   vbat = gainvbat(volt, r1, r2);
   duty = gainduty(pot);
   rms = gainrms(vbat, duty);
-
-  if (rms < 0.1 && digitalRead(fire) == HIGH ) {
+  res = gainres(ohmmet);
+  res -= 0.21; //For breadboard only. After soldering will be changed.
+  if(res > 9.99) res = 9.99;
+  if(res < 0) res = 0;
+  Serial.println(res);
+  /*if (rms < 0.1 && digitalRead(fire) == HIGH ) {
     tempnow = millis();
     while (digitalRead(fire) == HIGH) {}
     tempend = millis();
@@ -123,12 +130,12 @@ void loop() {
       if (mode == 1) mode = 0;
       else mode = 1;
     }
-  } else if (digitalRead(fire) == HIGH && rms >= 0.1) {
+    } else if (digitalRead(fire) == HIGH && rms >= 0.1) {
     tempnow = millis();
     while (millis() - tempnow < bouncedelay && digitalRead(fire) == HIGH) {}
     if (mode == 0 && digitalRead(fire) == HIGH) watt(mosfet, duty, fire);
     if (mode == 1 && digitalRead(fire) == HIGH) bypass(mosfet, fire);
-  }
+    }*/
   delay(10);
 }
 
@@ -143,7 +150,7 @@ void loop() {
   if (cbat < 70 && cbat >= 40) lcd.write(byte(2));
   if (cbat < 40 && cbat >= 15) lcd.write(byte(3));
   if (cbat < 15) lcd.write(byte(4));
-}*/
+  }*/
 
 void watt(int mostmp, float dutmp, int fitmp) { //mosfet, duty, fire
   float czas = millis();
@@ -161,7 +168,7 @@ void watt(int mostmp, float dutmp, int fitmp) { //mosfet, duty, fire
 void bypass(int mostmp, int fitmp) { //mosfet, fire
   float czas = millis();
   float licz = 0;
-  analogWrite(mostmp, 255);
+  analogWrite(mostmp, 1023);
   while (digitalRead(fitmp) != 0 && licz < 10)
     licz = (millis() - czas) / 1000;
   analogWrite(mostmp, 0.0);
@@ -182,7 +189,7 @@ int gainduty(float potmp) {
   float dutmp = analogRead(potmp);
   dutmp /= 4;
   dutmp = round(dutmp);
-  if (duty == 256) duty = 255;
+  if (dutmp >= 256) dutmp = 255;
   return (dutmp);
 }
 
@@ -190,6 +197,14 @@ float gainrms(float vbatmp, float dutmp) {
   float statmp = (dutmp * 100) / 255; //Stan w %
   float rmstmp = vbatmp * sqrt(statmp / 100); //RMS
   return (rmstmp);
+}
+
+float gainres(int ohmtmp) {
+  int val = analogRead(ohmtmp);
+  float volts = (val * 5) / 1023.0 ;
+  float resistance = volts * 8; //Set for 10ohm Vadj resistor for LM317
+  //float resistance = volts / 0.125; //Change 0.125 if needed
+  return (resistance);
 }
 
 void power() {
@@ -207,4 +222,5 @@ void poweron() {
   long int teemp = millis();
   while (digitalRead(fire) == HIGH) {}
   if ((millis() - teemp) >= 2000) on = true;
+  power();
 }
